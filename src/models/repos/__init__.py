fbe33b15369b63e38930a2e4a98a092598c5286c
averak <abe12@mccc.jp>
2020-08-14 10:52:01 +0900

    [refactor] repos/を整理

--- src/models/repos/__init__.py
+++ src/models/repos/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from .horse import HorseRepository
-from .jockey import JockeyRepository
+from .horse_repository import HorseRepository
+from .jockey_repository import JockeyRepository
 
 __all__ = [
     'HorseRepository',
