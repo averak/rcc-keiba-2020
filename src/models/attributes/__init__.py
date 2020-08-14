bcf0b4d641836c65a91bcfb0736188eb3a20894e
averak <abe12@mccc.jp>
2020-08-14 11:26:58 +0900

    [refactor] 動的にモジュールを読み込む
    [change] 属性オブジェクトの命名規則

--- src/models/attributes/__init__.py
+++ src/models/attributes/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from .id import ID
-from .name import Name
-from .birthday import Birthday
-from .weather import Weather
+import os
+import glob
+import inspect
 
-__all__ = [
-    'ID',
-    'Name',
-    'Birthday',
-    'Weather',
-]
+__all__ = []
+
+for file in glob.glob(os.path.dirname(__file__) + '/attr_*.py'):
+    module_name = os.path.basename(file).replace('.py', '')
+    exec('from . import {}'.format(module_name))
+    class_name = eval('inspect.getmembers(%s, inspect.isclass)[0][0]' % module_name)
+    exec('from .{} import {}'.format(module_name, class_name))
+    __all__.append(class_name)
