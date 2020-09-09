e89ffb17878f80d45d82a17c47b0ca0e4acd89c6
averak <abe12@mccc.jp>
2020-09-10 02:08:47 +0900

    [update] 出場馬一覧の長さバリデーションを追加

--- src/model/attribute/attr_horses.py
+++ src/model/attribute/attr_horses.py
@@ -1,9 +1,12 @@
 from . import attribute
 import model
 
+
 class Horses(attribute.Attribute):
+    max_size = 21
+
     def _validation_exception(self):
-        raise Exception('出場馬一覧はHorseオブジェクトの配列を指定してください')
+        raise Exception('出場馬一覧は長さ%d以下のHorseオブジェクトの配列を指定してください' % self.max_size)
 
     def _validation(self, attr):
         if attr is None:
@@ -13,6 +16,8 @@ class Horses(attribute.Attribute):
         for horse in attr:
             if type(horse) is not model.Horse:
                 return False
+        if len(attr) > self.max_size:
+            return False
 
         return True
 
