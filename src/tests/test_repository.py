e323d2ddc3dabdbc6be6501903fe6910fb4b357d
averak <abe12@mccc.jp>
2020-09-26 20:46:56 +0900

    [fix] マスタが空の場合はreturn

--- src/tests/test_repository.py
+++ src/tests/test_repository.py
@@ -7,6 +7,10 @@ class TestModel(unittest.TestCase):
         self.jockey_repo = repo.JockeyRepository()
 
     def test_jockey_repo(self):
+        if self.jockey_repo == []:
+            return
+
         jockey = self.jockey_repo[0]
         self.assertEqual([jockey], self.jockey_repo.find(jockey.id))
-        self.assertEqual([jockey], self.jockey_repo.find(jockey.name, attr='name'))
+        self.assertEqual([jockey], self.jockey_repo.find(
+            jockey.name, attr='name'))
